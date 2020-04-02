7fdb4b1724c46ea8f26c24c45fe7fd1e7a534cab
James Parsons <james@parsonsgreenmedia.com>
2020-04-02 19:24:44 +0100

    U

---
+++ app/Console/Commands/resetPassword.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class resetPassword extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'password:reset {user} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reset a users password';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        User::find($this->argument('user'))->update([
+            "password" => $this->argument('password')
+        ]);
+    }
+}
